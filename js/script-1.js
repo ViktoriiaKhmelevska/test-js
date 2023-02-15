341ff2fcae2a834338c7b5eda5e84fc8081c57d7
Viktoria Khmelevska <vikahvl@ukr.net>
2023-02-15 21:46:34 +0200

    simplified

--- js/script-1.js
+++ js/script-1.js
@@ -1,7 +1,18 @@
-const start = 3;
-const end = 7;
-
-for (let i = start; i <= end; i +=)
-{ 
-  console.log(i);
+function selectionSort(mas) {
+  var n = mas.length;
+  for (var i = 0; i < n - 1; i++) {
+    var min = i;
+    for (var j = i + 1; j < n; j++) {
+      if (mas[j] < mas[min]) min = j;
+    }
+    var t = mas[min];
+    mas[min] = mas[i];
+    mas[i] = t;
+  }
+  return mas;
 }
+
+var a = [10, 5, 8, 9, 1, 3, 2];
+a = selectionSort(a);
+// alert(a);
+console.log(a);
