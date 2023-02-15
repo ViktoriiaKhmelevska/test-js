0f58481e4df30477e29d7e2b91f6ba79e14ddbee
Viktoria Khmelevska <vikahvl@ukr.net>
2023-02-15 21:24:41 +0200

    update

--- js/script-1.js
+++ js/script-1.js
@@ -1,9 +1,7 @@
-const number = 10;
+const start = 3;
+const end = 7;
 
-for (let i = 0; i < number; i += 1) {
-  if (i % 2 === 0) {
-    continue;
-  }
-
-  console.log('Непарне i: ', i); // 1, 3, 5, 7, 9
+for (let i = start; i <= end; i +=)
+{ 
+  console.log(i);
 }
