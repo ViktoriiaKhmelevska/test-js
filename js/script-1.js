a9bdf35c55875d622c65f6abf4ce1385f28df2f5
Viktoria Khmelevska <vikahvl@ukr.net>
2023-02-17 00:57:26 +0200

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,18 +1,15 @@
-function selectionSort(mas) {
-  var n = mas.length;
-  for (var i = 0; i < n - 1; i++) {
-    var min = i;
-    for (var j = i + 1; j < n; j++) {
-      if (mas[j] < mas[min]) min = j;
-    }
-    var t = mas[min];
-    mas[min] = mas[i];
-    mas[i] = t;
+function calculateTotal(number) {
+ // Change code below this line
+  let sum = number;
+  let num = 0;
+  for (let i = 0; i < number || num < 0; i++) {
+  sum = sum + num;
+  num = number - 1;
   }
-  return mas;
+  console.log(sum);
+  return sum;
+  
+  // Change code above this line
 }
-
-var a = [10, 5, 8, 9, 1, 3, 2];
-a = selectionSort(a);
-// alert(a);
-console.log(a);
+calculateTotal(7);
+  // console.log(calculateTotal(21));
