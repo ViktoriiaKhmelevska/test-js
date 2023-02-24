d557d3eccaf7fd48fb03d2f76624935191b6a656
Viktoria Khmelevska <vikahvl@ukr.net>
2023-02-24 23:48:26 +0200

    hw3-2

--- js/script-1.js
+++ js/script-1.js
@@ -79,25 +79,50 @@
 // console.log(includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Jupiter'));
 // console.log(includes([3, 2, 3, 4, 5], 3));
   
-const colors = [
-  { hex: "#f44336", rgb: "244,67,54" },
-  { hex: "#2196f3", rgb: "33,150,243" },
-  { hex: "#4caf50", rgb: "76,175,80" },
-  { hex: "#ffeb3b", rgb: "255,235,59" },
-];
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
 
-const hexColors = [];
-const rgbColors = [];
-// Change code below this line
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
 
-  // // const cc = Object.values(colors.hex);
-  // console.log(cc);
-  for (let i = 0; i < colors.length; i += 1) {
-    hexColors.push(colors[i].hex);
-    rgbColors.push(colors[i].rgb);
-    // console.log(hexColors);
-    // console.log(rgbColors);
-  }
+//   // // const cc = Object.values(colors.hex);
+//   // console.log(cc);
+// //   for (let i = 0; i < colors.length; i += 1) {
+// //     hexColors.push(colors[i].hex);
+// //     rgbColors.push(colors[i].rgb);
+// //     // console.log(hexColors);
+// //     // console.log(rgbColors);
+// //   }
 
-   console.log(hexColors);
-    console.log(rgbColors);
+// //    console.log(hexColors);
+// //     console.log(rgbColors);
+
+//     const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//    let arr=[];
+//   for (let product of products)  {  
+
+//       if (product[propName])
+//       {
+//         arr.push(product[propName]);
+// }
+
+//   }
+//     return arr
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("category"));
