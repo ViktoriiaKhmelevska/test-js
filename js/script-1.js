b80d5c78d2d4456026b439dffb13aa7a955fe1a9
Viktoria Khmelevska <vikahvl@ukr.net>
2023-03-12 02:01:09 +0200

    add_test

--- js/script-1.js
+++ js/script-1.js
@@ -936,71 +936,71 @@
 // console.log(sortedByAscendingRating);
 // console.log(sortedByDescentingRating);
 
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    gender: "male"
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    gender: "female"
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male"
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    gender: "female"
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    gender: "male"
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    gender: "male"
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    gender: "female"
-  }
-];
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ];
 
 // const sortByDescendingFriendCount = users => {
 //   return [...users].sort((firstUser, secondUser) => firstUser.friends.length - secondUser.friends.length);
@@ -1157,27 +1157,27 @@ const users = [
 // Додаткове завдання
 // Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false). За замовчуванням значення властивості on повинно бути false.
 
-class Toggle {
-  constructor({isOpen: boolean}) {
-    this.isOpen = boolean;
-    this.on = false;
-  }
-}
+// class Toggle {
+//   constructor({isOpen: boolean}) {
+//     this.isOpen = boolean;
+//     this.on = false;
+//   }
+// }
 
-const firstToggle = new Toggle({ isOpen: true });
-console.log(firstToggle.isOpen);
-console.group('firstToggle');
-console.log(firstToggle.on);
-console.log(firstToggle.toggle);
-console.log(firstToggle.on);
-console.groupEnd('firstToggle');
+// const firstToggle = new Toggle({ isOpen: true });
+// console.log(firstToggle.isOpen);
+// console.group('firstToggle');
+// console.log(firstToggle.on);
+// console.log(firstToggle.toggle);
+// console.log(firstToggle.on);
+// console.groupEnd('firstToggle');
 
-const secondToggle = new Toggle();
-console.group('secondToggle');
-console.log(secondToggle.on);
-secondToggle.toggle();
-console.log(secondToggle.on);
-console.groupEnd('secondToggle');
+// const secondToggle = new Toggle();
+// console.group('secondToggle');
+// console.log(secondToggle.on);
+// secondToggle.toggle();
+// console.log(secondToggle.on);
+// console.groupEnd('secondToggle');
 
 // console.log("This is the outer level");
 // console.group();
@@ -1189,3 +1189,169 @@ console.groupEnd('secondToggle');
 // console.log("Back to level 2");
 // console.groupEnd();
 // console.log("Back to the outer level");
+
+// Homework:
+
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+
+// Task 1
+
+// Отримати масив імен усіх користувачів (поле name).
+
+const getUserNames = users => users.map(users => users.name);
+
+// console.log(getUserNames(users))
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// Task 2
+
+// Отримати масив об'єктів користувачей по кольору очей (поле eyeColor).
+const getUsersWithEyeColor = (users, color) => users.filter(users => users.eyeColor === color);
+    
+// console.log(getUsersWithEyeColor(users, 'blue')); // [об'є'кт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+
+// Task 3
+
+// Отримати загальну суму баланса (поле balance) усіх користувачів.
+const calculateTotalBalance = users => users.reduce((sum, user) => sum + user.balance, 0);
+// console.log(calculateTotalBalance(users)); // 20916
+
+// Task 4
+
+// Отримати масив імен користувачів по полю (поле gender)
+const getUsersWithGender = (users, genderType) => users.filter(users => users.gender === genderType).map(users => users.name);
+
+// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+
+const arr = [1, -3, -5, -2];
+
+// Дан массив с числами. Сделайте из него массив, состоящий из квадратов этих чисел.
+// console.log([1, 3, 5, 2].map(elem => elem * elem));
+
+//  Дан массив с числами. Найдите сумму этих чисел
+function summary(arr) {
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+return sum;
+}
+// console.log(summary(arr));
+
+// Дан массив с числами.Проверьте то, что все элементы в массиве больше нуля
+const everyPozitive = arr => {
+  const res = arr.every(arr => arr > 0);
+  console.log(res);
+  if (res) {
+    return "ALL element positive"
+  }
+ return "ERROR. NOT ALL element positive"
+}
+// console.log(everyPozitive(arr));
+
+//  Дан массив с числами. Проверьте то, что в нем есть отрицательные элементы
+const notEveryPozitive = arr => arr.some(arr => arr < 0) ? "Є відємні" : "Всі додатні";
+// console.log(notEveryPozitive(arr));
+
+// Дан массив с числами. Посчитайте количество отрицательных чисел в этом массиве.
+// const negative = arr => arr.filter(elem => elem < 0).length;
+// console.log(negative(arr));
+// console.log(arr.filter(elem => elem < 0).length);
+
+// document.getElementById("demo").innerHTML = myFunction();
+
+// const d = new Date();
+// alert(d);
+
+const d = new Date();
+// const year = d.getFullYear();
+// console.log(year);
+const nYear = d.setFullYear(2020);
+console.log(nYear);
+
